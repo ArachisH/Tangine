4a3a824b2f024ad430f2871bc5e772c09afbb96e
ArachisH <iKBenPeanut@gmail.com>
2022-04-10 20:40:15 -0700

    Created this type to provide information to the InterceptAsync method in HConnection

---
+++ Tangine/Network/HConnectionOptions.cs
@@ -0,0 +1,27 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+using Sulakore.Habbo;
+
+namespace Tangine.Network;
+
+public readonly record struct HConnectionOptions
+{
+    public static HConnectionOptions Default { get; }
+
+    public int ListenPort { get; init; } = 9567;
+    public int ListenSkipAmount { get; init; } = 0;
+
+    public bool IsUsingWebSockets { get; init; } = false;
+    public X509Certificate? Certificate { get; init; } = null;
+
+    public HConnectionOptions(IGame game)
+    {
+        if (game == null)
+        {
+            ThrowHelper.ThrowArgumentNullException(nameof(game));
+        }
+
+        // This may, or may not change in the future.
+        IsUsingWebSockets = game.Kind == GameKind.Unity;
+    }
+}
